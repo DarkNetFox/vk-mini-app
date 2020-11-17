ee44e1309dd50f0849c382e711694cedfb6d67c3
Sergey <35868005+DarkNetFox@users.noreply.github.com>
2020-11-17 19:08:58 +0600

    Add files via upload

---
+++ build/precache-manifest.bee15e5d947c37dd86650c2445928c59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb74666b40cba9a8b5f9c97c2753807",
+    "url": "/vk-mini-app/index.html"
+  },
+  {
+    "revision": "70d67037642410c66652",
+    "url": "/vk-mini-app/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "4bf695b87c5b3e8cafbe",
+    "url": "/vk-mini-app/static/css/main.aea4e6e6.chunk.css"
+  },
+  {
+    "revision": "70d67037642410c66652",
+    "url": "/vk-mini-app/static/js/2.77a3d2d7.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/vk-mini-app/static/js/2.77a3d2d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf695b87c5b3e8cafbe",
+    "url": "/vk-mini-app/static/js/main.8f9afa80.chunk.js"
+  },
+  {
+    "revision": "42612edfb5ed654f1347",
+    "url": "/vk-mini-app/static/js/runtime-main.0c4499f8.js"
+  }
+]);
